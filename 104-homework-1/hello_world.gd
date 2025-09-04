400940af0809448f56c3b53dc15352014428e59e
nebulaas-starbase <nebulaa@udel.edu>
2025-09-04 11:35:23 -0400

    wanted to add some extra

--- 104-homework-1/hello_world.gd
+++ 104-homework-1/hello_world.gd
@@ -3,3 +3,5 @@ extends RichTextLabel
 func _ready():
 	print("Hello world" + " ello guvnor")
 	#Underneath this line, write the line of code: print("Hello, World!")
+	if Input.is_action_pressed("ui_accept"):
+		print("squish effect")
